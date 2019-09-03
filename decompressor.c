522f35857df7950418cb41d58cae9a33cfc1e0d5
gideaopinheiro <gideao.eng@gmail.com>
2019-09-03 18:58:14 -0300

    construcao da funcao 'decompress()'

--- decompressor.c
+++ decompressor.c
@@ -21,12 +21,54 @@ int *getHeader(FILE *input_file)
 	return (header);
 }
 
-char *getTree()
+char *getTree(FILE *input_file, int len_tree){
+	BYTE *tree = malloc(len_tree);
+	fseek(input_file, 2, SEEK_SET);
+	for(int i=0; i < len_tree; i++){
+		fscanf(input_file, "%c", &tree[i]);
+		printf("%c", tree[i]);
+	}
+	printf("\n");
+	return (tree);	
+}
+
+TREE *buildTree(BYTE *tree, int *i){
+	TREE *new_node;
+	//new_node = malloc(sizeof(TREE));
+	
+	if(tree[*i] != '*'){
+		if(tree[*i] == '\\') *i += 1; 
+		new_node = create_node(tree[*i], 0);
+		*i += 1;
+
+		return new_node;
+	}
+	new_node = create_node(tree[*i], 0);
+	
+	*i += 1;
+	new_node->left = buildTree(tree, i);
+	new_node->right = buildTree(tree, i);
+	
+	return new_node;
+}
+
+void print_pre_order(TREE *tree){
+	if(tree != NULL){
+		printf("%c", *(char *)tree->item);
+		print_pre_order(tree->left);
+		print_pre_order(tree->right);
+	}
+	return;
+}
+
 
 void decompress_file(FILE *input_file)
 {
-	int *info = getHeader(input_file);
-	int *char = getTree(input_file, );
-	printf("%d: %d\n", info[0], info[1]);
+	int *info_header = getHeader(input_file);
+	BYTE *string_tree = getTree(input_file, info_header[1]);
+	printf("\n\n\n\n");
+	TREE *tree = buildTree(string_tree, 0);
+	print_pre_order(tree);
+	printf("\n");
 	return;
 }

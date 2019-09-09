d67be047f4779df7e5d1fa95696f1ffcdd52642d
gideaopinheiro <gideao.eng@gmail.com>
2019-09-09 16:59:01 -0300

    Update main.c

--- test_files/main.c
+++ test_files/main.c
@@ -111,7 +111,7 @@ int main(){
         return CU_get_error();
     }
 
-    CU_basic_set_mode(CU_BRM_VERBOSE); /*to check the parametres*/
+    CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
     
